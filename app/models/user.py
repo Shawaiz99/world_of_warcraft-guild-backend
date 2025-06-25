9f8a43bb25586bb12f22f911f87f2660b81558b3
Shawaiz <shawaizy2k@gmail.com>
2025-06-25 13:34:20 -0400

    Add guild model and user-guild relationship

--- app/models/user.py
+++ app/models/user.py
@@ -28,7 +28,8 @@ class User(db.Model):
     updated_at = mapped_column(
         DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     guild_id = mapped_column(Integer, ForeignKey("guilds.id"), nullable=True)
-    guild = relationship("Guild", back_populates="members")
+    guild = relationship("Guild", back_populates="members", foreign_keys=[guild_id])
+
 
     def serialize(self):
         return {
